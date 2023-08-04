24e6a93a8fda0546e0bf090dd4443a390cc1c5b5
k5020420 <k5020420@gmail.com>
2023-08-04 15:55:05 +0800

    0804_1555全完成的

---
+++ 全完成/menu.js
@@ -0,0 +1,16 @@
+// https://youtu.be/rm5lSQXoO4I?t=2100 JS教學部分
+// $(document).ready(function () {
+
+//     $('#collapse').on('click', function () {
+
+//         $('#menu').toggleClass('active');
+//     });
+
+// })
+console.log(document.querySelector("#collapse"))
+document.querySelector("#collapse").addEventListener("click",
+    function () {
+        console.log(document.querySelector("#menu").classList)
+        document.querySelector("#menu").classList.toggle('menu-active');
+    }
+);
